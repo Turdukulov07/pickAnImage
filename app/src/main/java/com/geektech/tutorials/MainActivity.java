88e2b091d9f25c2a0ddbea875c5662e1c1049b9f
Sovetbek <66789345+Turdukulov07@users.noreply.github.com>
2021-02-25 13:48:28 +0600

    pick an image

---
+++ app/src/main/java/com/geektech/tutorials/MainActivity.java
@@ -0,0 +1,52 @@
+package com.geektech.tutorials;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.net.URI;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int GALLERY_REQUEST_CODE = 123;
+
+    ImageView imageView;
+    Button btnPick;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.myImageView);
+        btnPick = findViewById(R.id.btnPickImage);
+
+
+        btnPick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Pick an image"), GALLERY_REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GALLERY_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            Uri imageData = data.getData();
+
+            imageView.setImageURI(imageData);
+        }
+    }
+
+}
